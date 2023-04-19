bb28bd9e7c6475b71523d5ace82d407aa68cb06e
Eugene <eugene@taranov.me>
2023-04-19 23:42:18 +0300

    upd amxage

--- pkg/server/service/loadbalancer/wrr/wrr.go
+++ pkg/server/service/loadbalancer/wrr/wrr.go
@@ -216,7 +216,7 @@ func (b *Balancer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	if b.stickyCookie != nil {
-		cookie := &http.Cookie{Name: b.stickyCookie.name, Value: server.name, Path: "/", HttpOnly: b.stickyCookie.httpOnly, Secure: b.stickyCookie.secure, MaxAge: 60}
+		cookie := &http.Cookie{Name: b.stickyCookie.name, Value: server.name, Path: "/", HttpOnly: b.stickyCookie.httpOnly, Secure: b.stickyCookie.secure, MaxAge: 30}
 		http.SetCookie(w, cookie)
 	}
 
